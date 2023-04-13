843bee7f929345021d3f78aeb21a9c9b2eaa6257
hectorbeltran-dev <128520652+hectorbeltran-dev@users.noreply.github.com>
2023-04-13 11:48:01 -0400

    Add project files.

---
+++ WebFormsSignalRChat.Module.Web/Controllers/ChatController.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Web;
+using WebFormsSignalRChat.Module.BusinessObjects;
+namespace WebFormsSignalRChat.Module.Web.Controllers
+{
+    public class ChatController : ObjectViewController<DetailView, ChatContainer>
+    {
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            ((WebWindow)this.Frame).RegisterStartupScript("test", "alert('test')");
+
+        }
+        public ChatController()
+        {
+            var sendMessage = new SimpleAction(this, "acSendMessage", "View");
+            sendMessage.Caption = "Send";
+            sendMessage.Execute += SendMessage_Execute;
+            
+        }
+
+        private void SendMessage_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            
+            //var currentObj = e.CurrentObject as ChatContainer;
+            //var hub = new ChatHub();
+            //hub.Send(currentObj.SenderName, currentObj.Message);
+        }
+       
+    }
+}
